1a6f69df40102763731cadb4051d7723fe845ba3
Eoin Lavery <dev@tenantc.co.uk>
2019-08-13 11:00:38 +0100

    Initial Commit

---
+++ OutaTime/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OutaTime
+//
+//  Created by Eoin Lavery on 13/08/2019.
+//  Copyright © 2019 Eoin Lavery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
